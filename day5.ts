465746f505a2f7c61486a64e9408105718563047
koen.ongena <gijoke@gmail.com>
2021-02-17 17:12:36 +0100

    Day 5 - part 2

--- day5.ts
+++ day5.ts
@@ -3,60 +3,111 @@ import {readFileContentForDay, splitNumberList} from "./input";
 
 const fileReader = (day) => () => readFileContentForDay(day);
 
-const firstParamPositionMode = (n) => (Math.floor(n / 100) % 10) == 0;
-const secondParamPositionMode = (n) => (Math.floor(n / 1000) % 10) == 0;
-
-const addition = (instructionCode) => R.curry((program, a, b, target) => {
-    const v1 = firstParamPositionMode(instructionCode) ? program[a] : a;
-    const v2 = secondParamPositionMode(instructionCode) ? program[b] : b;
-    return R.update(target, R.add(v1, v2), program);
+const addition = R.curry((program, index) => {
+    const v1 = getFirstParameter(program, index);
+    const v2 = getSecondParameter(program, index);
+    return R.update(program[index + 3], R.add(v1, v2), program);
 });
 
-const multiply = (instructionCode) => R.curry((program, a, b, target) => {
-    const v1 = firstParamPositionMode(instructionCode) ? program[a] : a;
-    const v2 = secondParamPositionMode(instructionCode) ? program[b] : b;
-    return R.update(target, R.multiply(v1, v2), program);
+const getFirstParameter = (p, index) => {
+    const instructionCode = p[index];
+    return (Math.floor(instructionCode / 100) % 10) == 0 ? p[p[index + 1]] : p[index + 1];
+}
+const getSecondParameter = (p, index) => {
+    const instructionCode = p[index];
+    return (Math.floor(instructionCode / 1000) % 10) == 0 ? p[p[index + 2]] : p[index + 2];
+}
+
+const multiply = R.curry((program, index) => {
+    const v1 = getFirstParameter(program, index);
+    const v2 = getSecondParameter(program, index);
+    return R.update(program[index + 3], R.multiply(v1, v2), program);
 });
 
-const input = (instructionCode) => R.curry((program, target) => {
-    return R.update(target, 1, program);
+const input = (inputValue) => R.curry((program, index) => {
+    const target = program[index + 1];
+    return R.update(target, inputValue, program);
 });
 
-const output = (instructionCode) => (program, target) => {
-    const value = firstParamPositionMode(instructionCode) ? program[target] : target;
-    console.log(value);
+const output = (program, index) => {
+    const value = getFirstParameter(program, index)
+    if (value !== 0) console.log(value);
     return program;
 };
 
-const getInstruction = (instructionCode: number) => {
-    if (instructionCode % 100 == 1) {
-        return addition(instructionCode);
-    } else if (instructionCode % 100 == 2) {
-        return multiply(instructionCode);
-    } else if (instructionCode % 100 == 3) {
-        return input(instructionCode)
-    } else if (instructionCode % 100 == 4) {
-        return output(instructionCode);
-    }
-}
-
-const getStep = (instructionCode: number) => {
-    let target = instructionCode % 100;
-    if (R.includes(target, [1, 2])) {
-        return 4;
-    }
-    return 2;
+const lessThan = (program, index) => {
+    const v1 = getFirstParameter(program, index);
+    const v2 = getSecondParameter(program, index);
+    return R.update(program[index + 3], v1 < v2 ? 1 : 0, program);
+};
+const eq = (program, index) => {
+    const v1 = getFirstParameter(program, index);
+    const v2 = getSecondParameter(program, index);
+    return R.update(program[index + 3], v1 === v2 ? 1 : 0, program);
 };
 
+const getInstruction = R.curry((inputValue, instructionCode: number) => {
+    const opCode = instructionCode % 100;
+    if (opCode == 1) {
+        return addition;
+    } else if (opCode == 2) {
+        return multiply;
+    } else if (opCode == 3) {
+        return input(inputValue);
+    } else if (opCode == 4) {
+        return output;
+    } else if (R.includes(opCode, [5, 6])) {
+        return R.identity;
+    } else if (opCode === 7) {
+        return lessThan;
+    } else if (opCode === 8) {
+        return eq;
+    } else {
+        throw new Error(opCode + " is not supported. Instruction code = " + instructionCode)
+    }
+});
+
+const jumpIfFirstParam = R.curry((predicate, program, index) => {
+    if (predicate(getFirstParameter(program, index))) {
+        return getSecondParameter(program, index);
+    }
+    return index + 3;
+});
+
+const jumpIfTrue = jumpIfFirstParam(R.complement(R.equals(0)));
+const jumpIfFalse = jumpIfFirstParam(R.equals(0));
+
+const getNextInstructionPointer = (program: number[], instructionCode: number, index: number) => {
+    const opcode = instructionCode % 100;
+    if (opcode === 5) {
+        return jumpIfTrue(program, index);
+    }
+    if (opcode === 6) {
+        return jumpIfFalse(program, index);
+    }
+    if (R.includes(opcode, [1, 2, 7, 8])) {
+        return index + 4;
+    }
+
+    return index + 2;
+};
+
+const determineInstruction = (program: number[], input = 1) => {
+    const stopState = ({program, index}) => program[index] == 99;
+    const instructionFor = getInstruction(input);
+
+    R.until(stopState, ({program, index}) => {
+        // console.log("At index " + index + " we find " + program[index]);
+        const instruction = instructionFor(program[index]);
+        const updatedProgram = instruction(program, index);
+        const nextInstructionPointer = getNextInstructionPointer(program, program[index], index);
+        return {program: updatedProgram, index: nextInstructionPointer}
+    })({program: program, index: 0});
+};
 (async () => {
 
     const program = await R.pipe(fileReader(5), R.andThen(splitNumberList()))()
 
-    const stopState = ({program, index}) => program[index] == 99;
-    R.until(stopState, ({program, index}) => {
-        const instruction = getInstruction(program[index]);
-        const updatedProgram = instruction(program, program[index + 1], program[index + 2], program[index + 3]);
-        const step = getStep(program[index]);
-        return {program: updatedProgram, index: index + step}
-    }, {program: program, index: 0});
+    determineInstruction(program, 1);
+    determineInstruction(program, 5);
 })();
